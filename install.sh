5727b9510bedca7e74abcddfd9831ec94a7f0fc7
x0xr00t <25748471+x0xr00t@users.noreply.github.com>
2021-03-10 14:55:30 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+read -p "Please enter the username: " user
+
+# Root check
+if [ "$EUID" -ne 0 ]
+  then echo -e "Please use ${RED_TEXT}root${GREEN_TEXT}for this installer"
+  exit
+fi
+
+# FlatPack install check
+flatpak="flatpak"
+if [ -x /bin/cmake ]; then
+    echo "{v} It appears that $flatpak exists :D"
+else
+    echo "{x} Arggg...It appears that $flatpak does not exist, we will install it."
+    apt install flatpak -y
+fi
+
+# Install the Software Flatpak plugin
+apt install gnome-software-plugin-flatpak -y
+
+# Add the Flathub repository
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+
+cd /home/$user/barrier-install
+flatpak install com.github.debauchee.barrier.flatpakref
+
+# reboot system to make it working 
+reboot
